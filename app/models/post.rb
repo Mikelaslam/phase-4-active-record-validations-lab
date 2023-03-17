665353650a569bbdfbbc380a48c99b96b2bda84b
MIKEL DAVIS <kipkorir385@gmail.com>
2023-03-17 13:03:47 +0300

    Completed task

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+  validate :clickbait
+
+  def clickbait
+    return if title.blank?
+
+    clickbait_patterns = [
+      /Won't Believe/i,
+      /Secret/i,
+      /Top \d+/i,
+      /Guess/i
+    ]
+
+    if clickbait_patterns.none? { |pattern| pattern.match(title) }
+      errors.add(:title, 'must be clickbait')
+    end
+  end
 end
+
